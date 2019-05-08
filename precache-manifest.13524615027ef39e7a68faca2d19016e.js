6bc8a773aa816a56b4ef7f22bc0745571a22b1b6
Cady Emad <cadyemad@Cadys-MacBook-Air.local>
2019-05-08 17:31:45 +0200

    Updates

---
+++ precache-manifest.13524615027ef39e7a68faca2d19016e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18ec3ee3c849e22d5765",
+    "url": "/VideoSearchApp-React/static/js/runtime~main.3f47381c.js"
+  },
+  {
+    "revision": "ff1eac132dc3f6a273f8",
+    "url": "/VideoSearchApp-React/static/js/main.cc123e8d.chunk.js"
+  },
+  {
+    "revision": "59dc26ba33e643cdaeb2",
+    "url": "/VideoSearchApp-React/static/js/2.62961acc.chunk.js"
+  },
+  {
+    "revision": "ff1eac132dc3f6a273f8",
+    "url": "/VideoSearchApp-React/static/css/main.94b03649.chunk.css"
+  },
+  {
+    "revision": "fdd99d7e63b341daeaea579dd326e32b",
+    "url": "/VideoSearchApp-React/index.html"
+  }
+];
